38a39d13d9466a95a623ab1bf328f27d8da147a3
the3darchprint <the3darchprint@gmail.com>
2025-02-05 09:53:54 +0100

    mappa letrehozas

--- mappa_creater/mappa_letrehozas.py
+++ mappa_creater/mappa_letrehozas.py
@@ -1,17 +1,14 @@
 import os
 
-# A fájl elérési útja (nevezzük "mappak.txt"-nek, és helyezd ugyanabba a mappába, mint a Python script)
-file_path = 'mappak.txt'
+file_path = 'C:\\Projects\\Coding\\python\\dxfwrite\\mappa_creater\\mappak.txt'
 
-# Az aktuális munkakönyvtár lekérése (ahol a script fut)
 base_directory = os.getcwd()
 
-# A fájl megnyitása és a mappanevek beolvasása
 with open(file_path, 'r', encoding='utf-8') as f:
     for line in f:
-        folder_name = line.strip()  # Eltávolítja a sortörést és egyéb felesleges karaktereket
-        if folder_name:  # Csak akkor hozzon létre mappát, ha a név nem üres
+        folder_name = line.strip() 
+        if folder_name:  
             folder_path = os.path.join(base_directory, folder_name)
-            os.makedirs(folder_path, exist_ok=True)  # Mappa létrehozása, ha nem létezik
+            os.makedirs(folder_path, exist_ok=True)  
 
-print(f"Mappák sikeresen létrehozva a következő helyen: {base_directory}")
+
